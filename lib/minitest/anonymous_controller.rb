b57f1bfcabed8042fb53a98ba8c23c2ecba47cea
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-12 00:01:49 +0900

    Add a method to insert dummy action

--- lib/minitest/anonymous_controller.rb
+++ lib/minitest/anonymous_controller.rb
@@ -1,7 +1,24 @@
-require "minitest/anonymous_controller/version"
+require 'minitest/anonymous_controller/version'
 
 module Minitest
   module AnonymousController
-    # Your code goes here...
+    def insert_dummy_action
+      # Define dummy action for testing in ApplicationController class context
+      self.class_eval do
+        def dummy_action
+          render :nothing
+        end
+      end
+
+      # Disable clrearing routes on Rails.application.routes.draw
+      ::Rails.application.routes.disable_clear_and_finalize = true
+
+      # Set a route for dummy action
+      ::Rails.application.routes.draw do
+        get 'dummy_action' => 'application#dummy_action'
+      end
+    end
   end
 end
+
+ActionController::Base.extend Minitest::AnonymousController
