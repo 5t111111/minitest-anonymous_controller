f34ff4801dbbb83e72ba1c3b888d241b203ed402
Hirofumi Wakasugi <baenej@gmail.com>
2016-05-11 23:32:32 +0900

    Initial commit

---
+++ lib/minitest/anonymous_controller.rb
@@ -0,0 +1,7 @@
+require "minitest/anonymous_controller/version"
+
+module Minitest
+  module AnonymousController
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'minitest/anonymous_controller'
+
+require 'minitest/autorun'
